842e6505ba6bbd2321eddba6c0e33becc693aea9
chris-cornish <csquare2390@gmail.com>
2017-01-09 14:57:31 -0500

    Created custom functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+def lumberjack(name, pronoun):
+        print("{} is a lumberjack and {} OK!".format(name, pronoun))
+
+lumberjack("Chris", "he's")
+lumberjack("Lacey", "she's")
+lumberjack("Sam", "they're")
+
+def average(num1, num2):
+    return (num1 + num2) / 2
+
+avg = average(2, 8)
+print(avg)
