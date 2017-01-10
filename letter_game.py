5b4dae98e3575cc9df2ee8c7a4000b81b3ea74a9
chris-cornish <csquare2390@gmail.com>
2017-01-10 09:55:36 -0500

    created a hangman game

--- letter_game.py
+++ letter_game.py
@@ -1,6 +1,63 @@
+import random
+
 # make a list of words
-# pick a random word
-# draw spaces
-# take guess
-# draw guess letters and strikes
-# print out win/lost
+words = [
+    'apple',
+    'banana',
+    'orange',
+    'coconut',
+    'strawberry',
+    'lime',
+    'grapefruit',
+    'lemon',
+    'kumquat',
+    'blueberry',
+    'melon'
+]
+
+while True:
+    start = input("Press enter/return to start, or enter Q to quit")
+    if start.lower() == 'q':
+        break
+
+    # pick a random word
+    secret_word = random.choice(words)
+    bad_guesses = []
+    good_guesses = []
+
+    while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
+        # draw spaces
+        # draw guess letters, spaces, and strikes
+        for letter in secret_word:
+            if letter in good_guesses:
+                print(letter, end='')
+            else:
+                print('_ ', end='')
+
+        print('')
+        print('Strikes: {}/7'.format(len(bad_guesses)))
+        print('')
+
+        # take guess
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1:
+            print("You can only guess a single letter!")
+            continue
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guessed that letter!")
+            continue
+        elif not guess.isalpha():
+            print("You can only guess letter!")
+            continue
+
+        # print out win/lost
+        if guess in secret_word:
+            good_guesses.append(guess)
+            if set(good_guesses) == set(secret_word):
+                print("You win! The word was {}".format(secret_word))
+                break
+        else:
+            bad_guesses.append(guess)
+    else:
+        print("You didn't guess it! My secret_word was {}".format(secret_word))
