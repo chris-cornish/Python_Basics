981dd848db5d8320e3b6f7d5ad1217c1030f2d95
chris-cornish <csquare2390@gmail.com>
2017-01-10 13:36:40 -0500

    created function to grab user guess

--- letter_game.py
+++ letter_game.py
@@ -39,33 +39,30 @@ def draw(bad_guesses, good_guesses, secret_word):
 
     print('')
 
+def get_guess(bad_guesses, good_guesses):
+    while True:
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1:
+            print("You can only guess a single letter!")
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guessed that letter!")
+        elif not guess.isalpha():
+            print("You can only guess letter!")
+        else:
+            return guess
+
 while True:
     start = input("Press enter/return to start, or enter Q to quit")
     if start.lower() == 'q':
         break
 
-    # pick a random word
     secret_word = random.choice(words)
     bad_guesses = []
     good_guesses = []
 
     while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
 
-
-        # take guess
-        guess = input("Guess a letter: ").lower()
-
-        if len(guess) != 1:
-            print("You can only guess a single letter!")
-            continue
-        elif guess in bad_guesses or guess in good_guesses:
-            print("You've already guessed that letter!")
-            continue
-        elif not guess.isalpha():
-            print("You can only guess letter!")
-            continue
-
-        # print out win/lost
         if guess in secret_word:
             good_guesses.append(guess)
             if set(good_guesses) == set(secret_word):
